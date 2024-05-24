663bd9d3ddd599ac0b7f6a1179484fea573b5bba
mahdimortuza <mahdi.webx@gmail.com>
2024-05-25 00:43:49 +0600

    shadcn installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
-  return <>Hello</>;
+  return (
+    <>
+      <h1 className="text-red-500 text-3xl">Hello</h1>
+      <Button>click</Button>
+    </>
+  );
 }
 
 export default App;
